911754f86a62b04b1704e6a64311fd9aae21850e
Isaac David Quintero Lobo <isaacquintero4k@gmail.com>
2025-07-16 15:45:43 -0500

    feat: create first drag cards and layout folders

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,21 @@
-import '../css/style.css'
+import "../css/style.css";
+import { CardComponent } from "../layout/card_ia/card";
+
+// Obtén el contenedor principal
+const app = document.getElementById("app");
+
+if (app) {
+  // Crea una card y agrégala al contenedor
+  const cardsData = [
+    {title:"card 1", content:"contenido dimanico"},
+  ];
+
+
+      
+  cardsData.forEach((data)=>{
+    const card = new CardComponent(data.title, data.content);
+    card.render(app);
+  })
+ 
+
+}
